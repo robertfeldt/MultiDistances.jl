64d2eb05da6c025168fe4d55e8151a2f67b401ea
Robert Feldt <robert.feldt@gmail.com>
2018-09-15 01:43:51 +0200

    generalize

--- src/utilities.jl
+++ src/utilities.jl
@@ -8,8 +8,8 @@ function file_distance(distance, file1, file2)
     1.0 - file_similarity(distance, file1, file2)
 end
 
-function find_most_similar(query::AbstractString, 
-    strings::Vector{AbstractString}, distance = Levenshtein())
+function find_most_similar(query::S, 
+    strings::Vector{S}, distance = Levenshtein()) where {S<:AbstractString}
 
     similarities = map(s -> compare(distance, s, query), strings)
     strings[last(findmax(similarities))]
