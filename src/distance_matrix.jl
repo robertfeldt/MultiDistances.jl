c643c7cf51a83440a353c01fd1a2816920e7ae8c
Robert Feldt <robert.feldt@gmail.com>
2019-04-01 09:38:42 +0200

    add precalc and progress meter

--- src/distance_matrix.jl
+++ src/distance_matrix.jl
@@ -1,11 +1,35 @@
-function distance_matrix(distance, strings::Vector{String})
+using ProgressMeter
+
+# Default is to not precalc. Override for Distances later.
+precalculate(dist, s) = s
+
+function distance_matrix(distance, strings::Vector{String}; 
+            showprogress = true,
+            precalc = true)
+
     n = length(strings)
     dm = zeros(Float64, n, n)
+
+    precalced = if precalc
+        map(s -> precalculate(distance, s), strings)
+    else
+        strings
+    end
+
+    p = if showprogress
+        numtotaldistances = n + div(n * (n - 1),  2)
+        Progress(numtotaldistances, 1.0, "Calculating distances...", 40)
+    end
+
     for i in 1:n
         for j in i:n
-            dm[i, j] = evaluate(distance, strings[i], strings[j])
-            dm[j, i] = dm[i, j]
+            dm[i, j] = dm[i, j] = evaluate(distance, precalced[i], precalced[j])
+            sleep(0.3)
+            if showprogress
+                next!(p)
+            end
         end
     end
-    dm
+
+    return dm
 end
