f301653b2b6990ecdba8e74e9381262842e85ad0
Francisco Neto <gomesf@chalmers.se>
2019-12-17 21:30:45 +0100

    Added two tests that break divseq.

--- test/test_diversity_sequence.jl
+++ test/test_diversity_sequence.jl
@@ -1,5 +1,5 @@
 using MultiDistances: MaxiMinDiversitySequence, DiversitySequence
-using MultiDistances: MaxiMeanDiversitySequence
+using MultiDistances: MaxiMeanDiversitySequence, find_maximean_sequence
 
 has_same_elements(a, b) = (length(a) == length(b)) && all(be -> in(be, a), b)
 
@@ -51,12 +51,29 @@ end
     # so the maximean sequence should be one of: [1, 3, 2, 4] or [3, 1, 2, 4] or [1, 4, 3, 2] or [4, 1, 3, 2]
     o = seq.order
     @test in(o, [[1, 3, 2, 4], [3, 1, 2, 4], [1, 4, 3, 2], [4, 1, 3, 2]])
+
+    # Distance Matrix for the next case below. Using Jaccard (2)
+    # Used Levenshtein since its easier to debug, and skip qgram sizes
+    other_objects = ["Hello", "Helo", "Oi", "Ola", "Heja"]
+    sequence = MaxiMeanDiversitySequence(Levenshtein(), other_objects)
+    object_ranking = sequence.order
+    # The first pair (highest distance) is added to first,
+    # and it could be: [a,b] or [b,a]
+    # Expected Result: ["Oi", "Hello", "Heja", "Ola", "Helo"]
+    @test in(object_ranking, [[3,1,5,4,2], [1,3,5,4,2]])
+
 end
 
 @testset "Few objects" begin
     d = Jaccard(2)
     @test_throws AssertionError MaxiMinDiversitySequence(d, [1])
     @test_throws AssertionError MaxiMeanDiversitySequence(d, [1])
+end
+
+@testset "Right matrix removal order" begin
+dm = [[0.0, 5.0, 10.0, 2.0, 1.0] [5.0, 0.0, 7.0, 1.0, 1.0] [10.0, 7.0, 0.0 ,1.0, 1.0] [ 2.0, 1.0, 1.0 ,0.0, 1.0] [ 2.0, 1.0, 1.0 ,1.0, 0.0]]
+object_ranking = find_maximean_sequence(dm)     
+@test in(object_ranking, [[1,3,2,4,5], [3,1,2,4,5]])
 end
 
 end
