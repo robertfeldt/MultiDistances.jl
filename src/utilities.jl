176181634d74ddca252e035cd77db0440af6020b
Robert Feldt <robert.feldt@gmail.com>
2022-01-05 15:35:06 +0100

    general add and subtract for dicts

--- src/utilities.jl
+++ src/utilities.jl
@@ -14,3 +14,34 @@ function find_most_similar(query::S,
     similarities = map(s -> compare(s, query, distance), strings)
     strings[last(findmax(similarities))]
 end
+
+function add!(d1::Dict{K,N}, d2::Dict{K,N}) where {K,N<:Number}
+    for (k,v) in d2
+        d1[k] = get(d1, k, 0) + v
+    end
+    return d1
+end
+
+add(d1::Dict{K,N}, d2::Dict{K,N}) where {K,N<:Number} = add!(deepcopy(d1), d2)
+
+function subtract!(d1::Dict{K,N}, d2::Dict{K,N}; 
+    minzero = false, clear = false) where {K,N<:Number}
+    for (k,v) in d2
+        currval = get(d1, k, 0)
+        if minzero
+            d1[k] = max(0, currval - v)
+        else
+            d1[k] = currval - v
+        end
+        if clear
+            if v >= currval
+                delete!(d1, k)
+            end
+        end
+    end
+    return d1
+end
+
+subtract(d1::Dict{K,N}, d2::Dict{K,N}; 
+    minzero = false, clear = false) where {K,N<:Number} = 
+    subtract!(deepcopy(d1), d2; minzero, clear)
--- test/test_utilities.jl
+++ test/test_utilities.jl
@@ -1,3 +1,4 @@
+using MultiDistances: add!, add, subtract!, subtract
 using StringDistances
 
 @testset "utilities" begin
@@ -23,6 +24,71 @@ end
 @testset "find_most_similar" begin
     @test MultiDistances.find_most_similar("a", ["a", "b"]) == "a"
     @test MultiDistances.find_most_similar("a", ["b3", "cd", "a2"]) == "a2"
+end
+
+@testset "dict add!" begin
+    d1 = Dict(:a => 1, :b => 2, :d => 5)
+    d2 = Dict(:a => 2, :b => 1, :c => 3, :d => 0)
+    d3 = add!(d1, d2)
+    @test d3 === d1
+    @test length(d3) == 4
+    @test d3[:a] == 3
+    @test d3[:b] == 3
+    @test d3[:c] == 3
+    @test d3[:d] == 5
+end
+
+@testset "dict add" begin
+    d1 = Dict("a" => 1, "b" => 2, "d" => 5)
+    d2 = Dict("a" => 2, "b" => 1, "c" => 3, "d" => 0)
+    d3 = add(d1, d2)
+    @test d3 !== d1
+    @test length(d3) == 4
+    @test d3["a"] == 3
+    @test d3["b"] == 3
+    @test d3["c"] == 3
+    @test d3["d"] == 5
+    @test d1["a"] == 1
+    @test d1["b"] == 2
+    @test d1["d"] == 5
+    @test !haskey(d1, "c")
+end
+
+@testset "dict subtract" begin
+    d1 = Dict(:a => 2, :b => 2, :d => 5)
+    d2 = Dict(:a => 1, :b => 2, :c => 3, :d => 0)
+
+    d3 = subtract(d1, d2)
+    @test d3 !== d1
+    @test length(d3) == 4 # a, b, c, d
+    @test d3[:a] == 1
+    @test d3[:b] == 0
+    @test d3[:c] == -3
+    @test d3[:d] == 5
+
+    d4 = subtract(d1, d2; minzero = true)
+    @test d4 !== d1
+    @test length(d4) == 4 # a, b, c, d
+    @test d4[:a] == 1
+    @test d4[:b] == 0
+    @test d4[:c] == 0
+    @test d4[:d] == 5
+
+    d5 = subtract(d1, d2; minzero = true, clear = true)
+    @test d5 !== d1
+    @test length(d5) == 2 # a, d since rest are 0
+    @test d5[:a] == 1
+    @test d5[:d] == 5
+    @test !haskey(d5, :b)
+    @test !haskey(d5, :c)
+end
+
+@testset "dict subtract!" begin
+    d1 = Dict(:a => 2, :b => 2, :d => 5)
+    d2 = Dict(:a => 1, :b => 2, :c => 3, :d => 0)
+    d3 = subtract!(d1, d2)
+    @test d3 === d1
+    @test length(d3) == 4
 end
 
 end
